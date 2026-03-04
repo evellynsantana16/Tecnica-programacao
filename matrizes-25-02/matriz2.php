98a96591b096f6227cbf73267b47bbeb49aa9ed1
evellynsantana16 <santanaevellyn16@gmail.com>
2026-03-04 09:31:36 -0300

    Trabalho- colocando media alunos

--- matrizes-25-02/matriz2.php
+++ matrizes-25-02/matriz2.php
@@ -1,18 +1,17 @@
 <?php
-//matriz de coluna nomeado e de linha ordenado
 $matriz2 = array(
-    array("nome" => "João", "p1" => 9.0, "p2" => 8.7, "t" => 4.0),   // Índice 0
-    array("nome" => "Silvia", "p1" => 8.0, "p2" => 8.1, "t" => 6.0), // Índice 1
-    array("nome" => "Pedro", "p1" => 3.0, "p2" => 4.7, "t" => 10.0), // Índice 2
-    array("nome" => "Tiago", "p1" => 9.0, "p2" => 5.5, "t" => 7.0),  // Índice 3
+    array("nome" => "João", "p1" => 9.0, "p2" => 8.7, "t" => 4.0),
+    array("nome" => "Silvia", "p1" => 8.0, "p2" => 8.1, "t" => 6.0),
+    array("nome" => "Pedro", "p1" => 3.0, "p2" => 4.7, "t" => 10.0),
+    array("nome" => "Tiago", "p1" => 9.0, "p2" => 5.5, "t" => 7.0),
 );
-echo "$matriz2[0]['nome']} <br>";
-foreach ($matriz2 as $vetor) {
-    // O segundo foreach pega os DADOS dentro dessa linha
-    foreach ($vetor as $dado) {
-        echo "$dado | ";
-    }
-    echo "<br>";
+
+foreach ($matriz2 as $aluno) {
+
+    $media = ($aluno["p1"] + $aluno["p2"] + $aluno["t"]) / 3;
+
+    echo "Aluno: " . $aluno["nome"] . "<br>";
+    echo "Média: " . number_format($media, 2) . "<br><br>";
 }
 
 ?>
